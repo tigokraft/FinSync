9d928863a366c5ebb96f6b7de3320e38efc7a145
exxo <exxo@DESKTOP-BGNG4S8>
2025-05-12 23:13:03 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(12, 12);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(208, 20);
+            this.txtuser.TabIndex = 0;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(12, 47);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(208, 20);
+            this.txtpass.TabIndex = 1;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(75, 89);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.TabIndex = 2;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 143);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button btnlogin;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient httpClient;
+        public Form1()
+        {
+            InitializeComponent();
+
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:5034")
+            };
+
+            httpClient.DefaultRequestHeaders.Add("x-api-key", "12345-abcdef-67890");
+        }
+
+        private async void btnlogin_Click(object sender, EventArgs e)
+        {
+            var credentials = new
+            {
+                username = txtuser.Text,
+                password = txtpass.Text
+            };
+
+            var json = JsonSerializer.Serialize(credentials);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await httpClient.PostAsync("api/auth/login", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var doc = JsonDocument.Parse(responseContent);
+                    var token = doc.RootElement.GetProperty("token").GetString();
+                    doc.Dispose();
+
+                    MessageBox.Show("Login successful!");
+
+                    // Store the token for future use
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                    // Example: Proceed to next form
+                    //var mainForm = new MainForm(httpClient);
+                    //mainForm.Show();
+                    //this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show($"Login failed: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error{ex.Message}");
+            }
+        }
+    }
+}
